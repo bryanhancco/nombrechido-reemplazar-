9ac1119be967b154d21fecac9a6649440bbeb2f6
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2022-04-28 23:03:52 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,8 +2,4 @@ class Calculator {
  	int add(int a, int b){ 
 		return a + b; 
 	}
-	int sub(int a, int b){ return 0; }
-	int mul(int a, int b){ return 0; }
-	int div(int a, int b){ return 0; }
-	int mod(int a, int b){ return 0; }
 }
